75f5e1218d0338d80de370dd41adcc3cb2b05e57
Daniel Kunz <daniel.kunz@de.bosch.com>
2016-07-21 14:28:13 +0200

    Updated mylyn script for Hudson.

--- build/org.eclipse.app4mc.build/hudson/init_mylyn.sh
+++ build/org.eclipse.app4mc.build/hudson/init_mylyn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-mkdir -p dev_env_lib/help_docu
-cp -v /shared/common/mylyn-wikitext/2.9.0.v20160513-1433/*.jar dev_env_lib/help_docu
+script_dir=$(dirname $0)
+folder_libs=$script_dir/../../../dev_env_lib/help_docu
+mkdir -p $folder_libs
+cp -v /shared/common/mylyn-wikitext/2.9.0.v20160513-1433/*.jar $folder_libs
