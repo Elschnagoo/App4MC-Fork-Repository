abeca665b3c8b79e63ddb12be1bb0f7fe061e611
Daniel Kunz <daniel.kunz@de.bosch.com>
2016-07-22 07:54:14 +0200

    Updated copy_release.sh to copy release and update site into dedicated folders. Fixed bug in reading tags to contain only tag for commit wwith --contains option.

--- build/org.eclipse.app4mc.build/hudson/copy_release.sh
+++ build/org.eclipse.app4mc.build/hudson/copy_release.sh
@@ -5,14 +5,19 @@ absolute=$(readlink -f $script_dir)
 release_build_folder=$absolute/../../org.eclipse.app4mc.platform.product/target/products
 p2_build_folder=$absolute/../../org.eclipse.app4mc.p2repo/target
 download_folder='/home/data/httpd/download.eclipse.org/app4mc'
+release_folder=$download_folder/releases
+updatesite_folder=$download_folder/updatesites
 commit_id=$(git rev-parse HEAD)
 echo 'Commit id is: '$commit_id
-tag_name=$(git describe --tags $commit_id)
+tag_name=$(git describe --tags --contains $commit_id)
 echo 'Tag name is: '$tag_name
 # Only copy to download area if tag is set in current commit
 if [ ${#tag_name} -gt 0 ]; then
-  echo 'Creating folder for tag '$tag_name
-  mkdir $download_folder/$tag_name
-  cp -v $release_build_folder/org.eclipse.app4mc.*.zip "$download_folder/$tag_name"
-  cp -v $p2_build_folder/org.eclipse.app4mc.p2repo*.zip "$download_folder/$tag_name"
+  echo 'Creating release folder for tag '$tag_name
+  mkdir -p $release_folder/$tag_name
+  cp -v $release_build_folder/org.eclipse.app4mc.*.zip "$release_folder/$tag_name"
+  cp -v $p2_build_folder/org.eclipse.app4mc.p2repo*.zip "$release_folder/$tag_name"
+  echo 'Creating updatesites folder for tag '$tag_name
+  mkdir -p $updatesite_folder/$tag_name
+  unzip $p2_build_folder/org.eclipse.app4mc.p2repo*.zip -d $updatesite_folder/$tag_name
 fi
