e006db91f86c7deba40da1520cef05c64f24d5d4
Daniel Kunz <daniel.kunz@de.bosch.com>
2016-07-27 10:01:14 +0200

    Updated script to copy products to products folder.

--- build/org.eclipse.app4mc.build/hudson/copy_release.sh
+++ build/org.eclipse.app4mc.build/hudson/copy_release.sh
@@ -5,7 +5,7 @@ absolute=$(readlink -f $script_dir)
 release_build_folder=$absolute/../../org.eclipse.app4mc.platform.product/target/products
 p2_build_folder=$absolute/../../org.eclipse.app4mc.p2repo/target
 download_folder='/home/data/httpd/download.eclipse.org/app4mc'
-release_folder=$download_folder/downloads
+product_folder=$download_folder/products
 updatesite_folder=$download_folder/updatesites
 commit_id=$(git rev-parse HEAD)
 echo 'Commit id is: '$commit_id
@@ -13,12 +13,12 @@ tag_name=$(git describe --tags --contains $commit_id)
 echo 'Tag name is: '$tag_name
 # Only copy to download area if tag is set in current commit
 if [ ${#tag_name} -gt 0 ]; then
-  echo 'Creating release folder for tag '$tag_name
-  mkdir -p $release_folder/$tag_name
-  cp -v $release_build_folder/org.eclipse.app4mc.*.zip "$release_folder/$tag_name"
-  cp -v $p2_build_folder/org.eclipse.app4mc.p2repo-*-SNAPSHOT-*.zip "$release_folder/$tag_name"
+  echo 'Creating product folder for tag '$tag_name
+  mkdir -p $product_folder/$tag_name
+  cp -v $release_build_folder/org.eclipse.app4mc.*.zip "$product_folder/$tag_name"
+  cp -v $p2_build_folder/org.eclipse.app4mc.p2repo-*-SNAPSHOT-*.zip "$product_folder/$tag_name"
   echo 'Removing SNAPSHOT in names...'
-  for file in "$release_folder/$tag_name"/*.zip; do
+  for file in "$product_folder/$tag_name"/*.zip; do
     mv -v "$file" "${file/-SNAPSHOT/}"
   done
   echo 'Creating updatesites folder for tag '$tag_name
