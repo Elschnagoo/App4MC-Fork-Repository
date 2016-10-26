443d48d0602f6c411e22007fff11b48f8bb539b4
Zakir Meer <zakirhussain.meer@de.bosch.com>
2016-10-26 23:22:11 +0200

    added junit TC for ModeSwitchEntry model migration scenario

--- tests/org.eclipse.app4mc.amalthea.converters.071.test/src/org/eclipse/app4mc/amalthea/converters071/test/SwConverterTest.java
+++ tests/org.eclipse.app4mc.amalthea.converters.071.test/src/org/eclipse/app4mc/amalthea/converters071/test/SwConverterTest.java
@@ -12,12 +12,17 @@ package org.eclipse.app4mc.amalthea.converters071.test;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.app4mc.amalthea.converters071.impl.HwConverter;
 import org.eclipse.app4mc.amalthea.converters071.impl.NamespaceConverter;
 import org.eclipse.app4mc.amalthea.converters071.impl.PropertyConstraintsConverter;
 import org.eclipse.app4mc.amalthea.converters071.impl.RootElementConverter;
 import org.eclipse.app4mc.amalthea.converters071.impl.SwConverter;
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,15 +34,22 @@ import org.junit.runners.Parameterized;
 public class SwConverterTest extends AbstractConverterTest {
 
 
-	@Parameterized.Parameters(name = "{index}: Test file: {0}")
+	@Parameterized.Parameters(name = "{index}: Test data ID: {0}")
 	public static Collection<Object[]> getTestData() {
 
-		return Arrays.asList(new Object[][] { { "/sw/AMALTHEA_Democar.amxmi", true } });
+
+		final String[] inputFiles_Section = new String[] { "/sw/AMALTHEA_Democar.amxmi" };
+
+		final String[] inputFiles_ModeSwitch = new String[] { "/sw/modeswitch/modeswitch.amxmi",
+				"/sw/modeswitch/modeliterals.amxmi" };
+
+		return Arrays.asList(new Object[][] { { "Sections : 1", true, inputFiles_Section },
+				{ "ModeSwitchEntry_Scenario : 2", true, inputFiles_ModeSwitch } });
 	}
 
-	public SwConverterTest(final String xmlFileRelativeLocation, final boolean canExecuteTestCase) {
+	public SwConverterTest(final String testDataID, final boolean canExecuteTestCase, final String[] xmlFiles) {
 
-		super(xmlFileRelativeLocation, canExecuteTestCase);
+		super(testDataID, xmlFiles, canExecuteTestCase);
 	}
 
 
@@ -52,6 +64,35 @@ public class SwConverterTest extends AbstractConverterTest {
 	@Test
 	public void verification() {
 		super.verification();
+
+		verify_modeswitchEntry();
+	}
+
+	private void verify_modeswitchEntry() {
+
+		for (final Document document : this.fileName_documentsMap.values()) {
+
+			final Element rootElement = document.getRootElement();
+
+			final List<Element> xpathResult = this.helper.getXpathResult(rootElement,
+					".//callGraph//graphEntries//entries[@value]|.//callGraph//graphEntries//entries/value|.//callGraph//graphEntries//entries[@default]",
+					Element.class, this.helper.getNS_071("am"));
+
+			if (xpathResult.size() > 0) {
+
+				final List<Attribute> xpathResult2 = this.helper.getXpathResult(xpathResult.get(0),
+						"./ancestor::tasks[1]/@name", Attribute.class);
+
+
+				Assert.assertTrue(
+						"Task \"" + xpathResult2.get(0).getValue()
+								+ "\" is not migrated completely.. and is not valid as per 0.7.1",
+						xpathResult.size() == 0);
+			}
+
+
+		}
+
 	}
 
 
