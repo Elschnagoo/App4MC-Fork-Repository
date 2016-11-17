9b0f0180dab643da2cfe912dd16acfe4fbfb1c88
Harald Mackamul <harald.mackamul@de.bosch.com>
2016-11-17 12:37:18 +0100

    Added copyright headers for validations

--- tests/org.eclipse.app4mc.amalthea.validation.test/src/org/eclipse/app4mc/amalthea/validation/ta/checks/impl/OSModelValidatorTests.java
+++ tests/org.eclipse.app4mc.amalthea.validation.test/src/org/eclipse/app4mc/amalthea/validation/ta/checks/impl/OSModelValidatorTests.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Timing-Architects Embedded Systems GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Timing-Architects Embedded Systems GmbH - initial API and implementation
+ *
+ *******************************************************************************/
+
 package org.eclipse.app4mc.amalthea.validation.ta.checks.impl;
 
 import org.easymock.EasyMock;
