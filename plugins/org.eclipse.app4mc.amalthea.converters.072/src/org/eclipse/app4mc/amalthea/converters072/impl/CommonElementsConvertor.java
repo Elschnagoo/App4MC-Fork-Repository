80599a5fde43217a03a0c3dd88a17156b39a81ce
Zakir Meer <zakirhussain.meer@de.bosch.com>
2017-01-11 13:50:35 +0100

    Bug 510086 : migration of Tag elements to CommonElements + JUnit test cases for verifiying migration

---
+++ plugins/org.eclipse.app4mc.amalthea.converters.072/src/org/eclipse/app4mc/amalthea/converters072/impl/CommonElementsConvertor.java
@@ -0,0 +1,78 @@
+package org.eclipse.app4mc.amalthea.converters072.impl;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.eclipse.app4mc.amalthea.converters.common.base.ICache;
+import org.eclipse.app4mc.amalthea.converters072.utils.HelperUtils_071_072;
+import org.jdom2.Document;
+import org.jdom2.Element;
+
+public class CommonElementsConvertor extends AbstractConverter {
+
+
+	public CommonElementsConvertor() {
+		this.helper = HelperUtils_071_072.getInstance();
+		this.logger = LogManager.getLogger(this.getClass());
+	}
+
+
+	@Override
+	public void convert(final File targetFile, final Map<File, Document> fileName_documentsMap,
+			final List<ICache> caches) throws Exception {
+
+		this.logger.info("Migration from 0.7.1 to 0.7.2 : Starting \"Common Elements\" converter");
+
+		final Document root = fileName_documentsMap.get(targetFile);
+
+		if (root == null) {
+			return;
+		}
+		final Element rootElement = root.getRootElement();
+
+		updateCommonElements(rootElement);
+	}
+
+
+	/**
+	 * Bug 510086 : As per the model changes in 0.7.2 ==>
+	 *
+	 * @param rootElement
+	 */
+	private void updateCommonElements(final Element rootElement) {
+
+		final StringBuffer xpathBuffer = new StringBuffer();
+
+		xpathBuffer.append("./componentsModel/tags");
+		xpathBuffer.append("|");
+		xpathBuffer.append("./hwModel/tags");
+		xpathBuffer.append("|");
+		xpathBuffer.append("./swModel/tags");
+
+		final List<Element> tagElements = this.helper.getXpathResult(rootElement, xpathBuffer.toString(), Element.class,
+				this.helper.getGenericNS("xsi"));
+
+		if (tagElements.size() > 0) {
+
+			Element commonElements = rootElement.getChild("commonElements");
+
+			// check if "commonElements" tag is already existing. If not then create a Element
+			if (commonElements == null) {
+				commonElements = new Element("commonElements");
+				rootElement.addContent(commonElements);
+			}
+
+			for (final Element tagElement : tagElements) {
+				final Element parentElement = tagElement.getParentElement();
+				// removing Tag Elements from its parent and associating it to commonElements Tag
+				parentElement.removeContent(tagElement);
+				commonElements.addContent(tagElement);
+			}
+		}
+
+
+	}
+
+}
---
+++ tests/org.eclipse.app4mc.amalthea.converters.072.test/src/org/eclipse/app4mc/amalthea/converters072/test/CommonElementsConverterTest.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Robert Bosch GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Bosch GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.app4mc.amalthea.converters072.test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.app4mc.amalthea.converters072.impl.CommonElementsConvertor;
+import org.eclipse.app4mc.amalthea.converters072.impl.NamespaceConverter;
+import org.eclipse.app4mc.amalthea.converters072.impl.OSConverter;
+import org.eclipse.app4mc.amalthea.converters072.impl.StimuliConvertor;
+import org.eclipse.app4mc.amalthea.converters072.impl.SwConverter;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(Parameterized.class)
+public class CommonElementsConverterTest extends AbstractConverterTest {
+
+
+	@Parameterized.Parameters(name = "{index}: Test file: {0}")
+	public static Collection<Object[]> getTestData() {
+
+		return Arrays.asList(new Object[][] { { "/commonElements/commonElements.amxmi", true } });
+	}
+
+	public CommonElementsConverterTest(final String xmlFileRelativeLocation, final boolean canExecuteTestCase) {
+
+		super(xmlFileRelativeLocation, canExecuteTestCase);
+	}
+
+	@Test
+	public void testConversion() {
+		super.testConversion(NamespaceConverter.class, OSConverter.class, SwConverter.class, StimuliConvertor.class,
+				CommonElementsConvertor.class);
+
+		verify_Tag_Elements();
+	}
+
+
+	private void verify_Tag_Elements() {
+
+		final StringBuffer xpathBuffer = new StringBuffer();
+
+		xpathBuffer.append("./componentsModel/tags");
+		xpathBuffer.append("|");
+		xpathBuffer.append("./hwModel/tags");
+		xpathBuffer.append("|");
+		xpathBuffer.append("./swModel/tags");
+
+
+		for (final Document document : this.fileName_documentsMap.values()) {
+
+			final Element rootElement = document.getRootElement();
+
+			final List<Element> xpathResult = this.helper.getXpathResult(rootElement, xpathBuffer.toString(),
+					Element.class, this.helper.getNS_072("am"), this.helper.getGenericNS("xsi"));
+
+			if (xpathResult.size() > 0) {
+
+				Assert.assertTrue(
+						"Tag elements content migration is not successful --> as still it contains \"Tag elements inside -> componentsModel or hwModel or swModel \". For further analysis check the migrated model file :"
+								+ document.getBaseURI(),
+						xpathResult.size() == 0);
+
+			}
+
+
+		}
+
+
+	}
+
+
+	@Override
+	@Test
+	public void verification() {
+		super.verification();
+
+
+	}
+
+
+}
