eb42cd47bf2589dddfca25846f43e553f9315126
Zakir Meer <zakirhussain.meer@de.bosch.com>
2017-01-11 13:51:28 +0100

    Bug 510088 : migration of ArrivalCurve: "arrivalCurveEntries" -> "entries" + JUnit test cases for verifiying migration

---
+++ plugins/org.eclipse.app4mc.amalthea.converters.072/src/org/eclipse/app4mc/amalthea/converters072/impl/StimuliConvertor.java
@@ -0,0 +1,61 @@
+package org.eclipse.app4mc.amalthea.converters072.impl;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.eclipse.app4mc.amalthea.converters.common.base.ICache;
+import org.eclipse.app4mc.amalthea.converters072.utils.HelperUtils_071_072;
+import org.jdom2.Document;
+import org.jdom2.Element;
+
+public class StimuliConvertor extends AbstractConverter {
+
+
+	public StimuliConvertor() {
+		this.helper = HelperUtils_071_072.getInstance();
+		this.logger = LogManager.getLogger(this.getClass());
+	}
+
+
+	@Override
+	public void convert(final File targetFile, final Map<File, Document> fileName_documentsMap,
+			final List<ICache> caches) throws Exception {
+
+		this.logger.info("Migration from 0.7.1 to 0.7.2 : Starting Stimuli converter");
+
+		final Document root = fileName_documentsMap.get(targetFile);
+
+		if (root == null) {
+			return;
+		}
+		final Element rootElement = root.getRootElement();
+
+		updateArrivalCurve(rootElement);
+	}
+
+
+	/**
+	 * Bug 510088 : As per the model changes in 0.7.2 ==> "arrivalCurveEntries" is changed to "entries" inside the class
+	 * ArrivalCurve
+	 *
+	 * @param rootElement
+	 */
+	private void updateArrivalCurve(final Element rootElement) {
+
+		final StringBuffer xpathBuffer = new StringBuffer();
+
+		xpathBuffer.append("./stimuliModel/stimuli[@xsi:type=\"am:ArrivalCurve\"]/arrivalCurveEntries");
+
+		final List<Element> elements = this.helper.getXpathResult(rootElement, xpathBuffer.toString(), Element.class,
+				this.helper.getGenericNS("xsi"));
+
+		for (final Element element : elements) {
+			// changing tag name from : arrivalCurveEntries to entries
+			element.setName("entries");
+		}
+
+	}
+
+}
---
+++ tests/org.eclipse.app4mc.amalthea.converters.072.test/src/org/eclipse/app4mc/amalthea/converters072/test/StimuliConverterTest.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Robert Bosch GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Bosch GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.app4mc.amalthea.converters072.test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.app4mc.amalthea.converters072.impl.NamespaceConverter;
+import org.eclipse.app4mc.amalthea.converters072.impl.OSConverter;
+import org.eclipse.app4mc.amalthea.converters072.impl.StimuliConvertor;
+import org.eclipse.app4mc.amalthea.converters072.impl.SwConverter;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(Parameterized.class)
+public class StimuliConverterTest extends AbstractConverterTest {
+
+
+	@Parameterized.Parameters(name = "{index}: Test file: {0}")
+	public static Collection<Object[]> getTestData() {
+
+		return Arrays.asList(new Object[][] { { "/stimuli/Stimuli.amxmi", true } });
+	}
+
+	public StimuliConverterTest(final String xmlFileRelativeLocation, final boolean canExecuteTestCase) {
+
+		super(xmlFileRelativeLocation, canExecuteTestCase);
+	}
+
+	@Test
+	public void testConversion() {
+		super.testConversion(NamespaceConverter.class, OSConverter.class, SwConverter.class, StimuliConvertor.class);
+
+		verify_ArrivalCurveEntries();
+	}
+
+
+	/**
+	 * Bug 510088 : As per the changes in MetaModel - > arrivalCurveEntries tag is changed to entries.
+	 *
+	 * After successful model migration -> there should not be any arrivalCurveEntries tags inside ArrivalCurve Stimulus
+	 * element.
+	 *
+	 * Below method is used to verify if there exists any arrivalCurveEntries tags inside Stimulus (ArrivalCurve)
+	 * element .
+	 *
+	 */
+	private void verify_ArrivalCurveEntries() {
+
+		final StringBuffer xpathBuffer = new StringBuffer();
+		xpathBuffer.append("./stimuliModel/stimuli[@xsi:type=\"am:ArrivalCurve\"]/arrivalCurveEntries");
+
+
+		for (final Document document : this.fileName_documentsMap.values()) {
+
+			final Element rootElement = document.getRootElement();
+
+			final List<Element> xpathResult = this.helper.getXpathResult(rootElement, xpathBuffer.toString(),
+					Element.class, this.helper.getNS_072("am"), this.helper.getGenericNS("xsi"));
+
+			if (xpathResult.size() > 0) {
+				Assert.assertTrue(
+						"ArrivalCurve Stimulus element content migration is not successful --> as still it contains \"arrivalCurveEntries tag\". For further analysis check the migrated model file :"
+								+ document.getBaseURI(),
+						xpathResult.size() == 0);
+
+			}
+
+
+		}
+
+
+	}
+
+
+	@Override
+	@Test
+	public void verification() {
+		super.verification();
+
+
+	}
+
+
+}
