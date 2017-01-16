cfbd9d049955d5b48719d7097cfd3e14a5c32f5c
Zakir Meer <zakirhussain.meer@de.bosch.com>
2017-01-16 20:27:42 +0100

    updated header info

--- plugins/org.eclipse.app4mc.amalthea.converters.072/src/org/eclipse/app4mc/amalthea/converters072/impl/CommonElementsConvertor.java
+++ plugins/org.eclipse.app4mc.amalthea.converters.072/src/org/eclipse/app4mc/amalthea/converters072/impl/CommonElementsConvertor.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Robert Bosch GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Bosch GmbH - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.app4mc.amalthea.converters072.impl;
 
 import java.io.File;
