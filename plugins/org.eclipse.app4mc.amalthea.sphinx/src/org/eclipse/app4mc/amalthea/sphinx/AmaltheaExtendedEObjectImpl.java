b31101eebc25282dbaf0abbf75b810bf26812df4
Harald Mackamul <harald.mackamul@de.bosch.com>
2018-05-28 15:46:18 +0200

    Set MinimalEObject as base implementation class for AMALTHEA objects

--- plugins/org.eclipse.app4mc.amalthea.sphinx/src/org/eclipse/app4mc/amalthea/sphinx/AmaltheaExtendedEObjectImpl.java
+++ plugins/org.eclipse.app4mc.amalthea.sphinx/src/org/eclipse/app4mc/amalthea/sphinx/AmaltheaExtendedEObjectImpl.java
@@ -1,6 +1,6 @@
 /**
  * *******************************************************************************
- *  Copyright (c) 2015 Robert Bosch GmbH and others.
+ *  Copyright (c) 2015, 2018 Robert Bosch GmbH and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -14,38 +14,20 @@
 
 package org.eclipse.app4mc.amalthea.sphinx;
 
-import org.eclipse.sphinx.emf.ecore.ExtendedEObjectImpl;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+import org.eclipse.sphinx.emf.ecore.proxymanagement.ProxyResolutionBehavior;
 
 
-public class AmaltheaExtendedEObjectImpl extends ExtendedEObjectImpl {
+public class AmaltheaExtendedEObjectImpl extends MinimalEObjectImpl.Container {
 
-//	/**
-//	 * @see org.eclipse.emf.common.notify.impl.BasicNotifierImpl#eNotify(org.eclipse.emf.common.notify.Notification)
-//	 */
-//	@Override
-//	public void eNotify(final Notification notification) {
-//		super.eNotify(notification);
-//
-//		// Send additional notifications
-//		if (containerNotificationRequired() && (eContainer() != null)) {
-//			final Notification n = new ENotificationImpl((InternalEObject) eContainer(), Notification.SET,
-//					eContainingFeature(), this, this);
-//
-//			eContainer().eNotify(n);
-//		}
-//	}
-//
-//	/**
-//	 * @see org.eclipse.emf.common.notify.impl.BasicNotifierImpl#eNotificationRequired()
-//	 */
-//	@Override
-//	public boolean eNotificationRequired() {
-//		return super.eNotificationRequired();
-//	}
-//
-//	// default implementation
-//	public boolean containerNotificationRequired() {
-//		return false;
-//	}
+	/*
+	 * @see org.eclipse.emf.ecore.impl.BasicEObjectImpl#eResolveProxy(org.eclipse.emf.ecore.InternalEObject)
+	 */
+	@Override
+	public EObject eResolveProxy(final InternalEObject proxy) {
+		return ProxyResolutionBehavior.INSTANCE.eResolveProxy(this, proxy);
+	}
 
 }
