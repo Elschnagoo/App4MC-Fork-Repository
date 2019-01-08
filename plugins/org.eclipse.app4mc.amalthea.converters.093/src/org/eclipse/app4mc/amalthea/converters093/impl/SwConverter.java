7a32b645db9d28f53f7ce1fa3af381850346d2f3
Zakir Meer <zakirhussain.meer@de.bosch.com>
2019-01-08 16:35:45 +0100

    updated migration of ExecutionNeed element (inside Runnable, TaskScheduler, InterruptController)

--- plugins/org.eclipse.app4mc.amalthea.converters.093/src/org/eclipse/app4mc/amalthea/converters093/impl/SwConverter.java
+++ plugins/org.eclipse.app4mc.amalthea.converters.093/src/org/eclipse/app4mc/amalthea/converters093/impl/SwConverter.java
@@ -1,6 +1,7 @@
 package org.eclipse.app4mc.amalthea.converters093.impl;
 
 import java.io.File;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -150,172 +151,239 @@ public class SwConverter extends AbstractConverter {
 
 		xpathBuffer.append("./swModel/runnables//*[@xsi:type=\"am:ExecutionNeed\"]");
 		xpathBuffer.append("|");
-		xpathBuffer.append("./osModel/operatingSystems/taskSchedulers/computationItems[@xsi:type=\"am:ExecutionNeed\"]");
+		xpathBuffer
+				.append("./osModel/operatingSystems/taskSchedulers/computationItems[@xsi:type=\"am:ExecutionNeed\"]");
 		xpathBuffer.append("|");
-		xpathBuffer.append("./osModel/operatingSystems/interruptControllers/computationItems[@xsi:type=\"am:ExecutionNeed\"]");
-		 
+		xpathBuffer.append(
+				"./osModel/operatingSystems/interruptControllers/computationItems[@xsi:type=\"am:ExecutionNeed\"]");
 
-		final List<Element> executionNeeds = this.helper.getXpathResult(rootElement, xpathBuffer.toString(), Element.class,
-				this.helper.getNS_093("am"),this.helper.getGenericNS("xsi"));
+		final List<Element> executionNeeds = this.helper.getXpathResult(rootElement, xpathBuffer.toString(),
+				Element.class, this.helper.getNS_093("am"), this.helper.getGenericNS("xsi"));
 
 		for (final Element executionNeedElement : executionNeeds) {
-			
-			
-			String containerElementName=null;
-			
-			String containerType="";
-			
-			Element parentElement = this.helper.getParentElementOfName(executionNeedElement, "runnables","taskSchedulers","interruptControllers");
-			
-			if(parentElement !=null) {
-				
-				containerElementName=parentElement.getAttributeValue("name");
-				
-				if(parentElement.getName().equals("runnables")) {
-					containerType="Runnable";
-				}else if(parentElement.getName().equals("taskSchedulers")) {
-					containerType="TaskScheduler";
-					
-				}else if(parentElement.getName().equals("interruptControllers")) {
-					containerType="InterruptController";
-					
+
+			String containerElementName = null;
+
+			String containerType = "";
+
+			Element parentElement = this.helper.getParentElementOfName(executionNeedElement, "runnables",
+					"taskSchedulers", "interruptControllers");
+
+			if (parentElement != null) {
+
+				containerElementName = parentElement.getAttributeValue("name");
+
+				if (parentElement.getName().equals("runnables")) {
+					containerType = "Runnable";
+				} else if (parentElement.getName().equals("taskSchedulers")) {
+					containerType = "TaskScheduler";
+
+				} else if (parentElement.getName().equals("interruptControllers")) {
+					containerType = "InterruptController";
+
 				}
 			}
-		 
-			String nodeName=executionNeedElement.getName();
-			
-			
+
+			String nodeName = executionNeedElement.getName();
+
 			/*-
 			 *    <default key="Instructions">
-          <value xsi:type="am:NeedConstant" value="45"/>
-        </default>
-        <default key="FLOP">
-          <value xsi:type="am:NeedDeviation">
-            <deviation>
-              <lowerBound xsi:type="am:LongObject" value="0"/>
-              <upperBound xsi:type="am:LongObject" value="0"/>
-              <distribution xsi:type="am:BetaDistribution" alpha="0.0" beta="0.0"/>
-            </deviation>
-          </value>
-        </default>
+			<value xsi:type="am:NeedConstant" value="45"/>
+			</default>
+			<default key="FLOP">
+			<value xsi:type="am:NeedDeviation">
+			<deviation>
+			  <lowerBound xsi:type="am:LongObject" value="0"/>
+			  <upperBound xsi:type="am:LongObject" value="0"/>
+			  <distribution xsi:type="am:BetaDistribution" alpha="0.0" beta="0.0"/>
+			</deviation>
+			</value>
+			</default>
 			 */
 			List<Element> defaultElements = executionNeedElement.getChildren("default");
-			
-			boolean isInstructionsPresent=false;
 
-			Element defaultInstructionsElement=null;
-			
-			loop: for (Element defaultElement : defaultElements) {
-				
+			Map<String, Element> defaultElementsMap = new LinkedHashMap<>();
+
+			for (Element defaultElement : defaultElements) {
+
 				String key = defaultElement.getAttributeValue("key");
-				
-				if(key!=null && key.equalsIgnoreCase("Instructions")) {
-					isInstructionsPresent=true;
-					defaultInstructionsElement=defaultElement;
-					break loop;
-				}
-			}
-			
-			/*- ------ExecutionTicks element --------------*/
-			Element executionTicksElement=new Element(nodeName);
-			
-			Attribute typeAttribute=new Attribute("type", "am:Ticks", this.helper.getGenericNS("xsi"));
-			
-			executionTicksElement.getAttributes().add(typeAttribute);
-			
-			/*- -----------------------------------------*/
-			
-			if(isInstructionsPresent && defaultInstructionsElement!=null) {
-				
-				/*-
-				 *    <default key="Instructions">
-          				<value xsi:type="am:NeedConstant" value="20"/>
-        			  </default>
-				 */
-				Element valueElement = defaultInstructionsElement.getChild("value");
-	
-				Element default_executionTicksElement= createTicksElementFromNeed( valueElement,"default");
-	
-				if(default_executionTicksElement!=null) {
-					//Adding default ExecutionTicks element
-					executionTicksElement.addContent(default_executionTicksElement);
-				}
-				
-			}
-			
-			/*------------------------------ Handling ExtendedElements ---------------------------*/
-			
-			
-			List<Element> extendedElements = executionNeedElement.getChildren("extended");
-			
-			for (Element extendedElement : extendedElements) {
-			
-				
 
-				final List<Element> valueElements = this.helper.getXpathResult(extendedElement,"./value[@key=\"Instructions\"]", Element.class,
-						this.helper.getNS_093("am"));
-				
-				if(valueElements.size()>0) {
-					
-					Element extended_executionTicksElement=new Element("extended");
-					
-					this.helper.copyElement_Attribute_or_Element(extendedElement,extended_executionTicksElement,"key");
+				if (key != null) {
+					defaultElementsMap.put(key, defaultElement);
+				}
+			}
+
+			Element newExecutionTicksElement = null;
+
+			Element newExecutionNeedsElement = null;
+
+			for (String key : defaultElementsMap.keySet()) {
+
+				if (key.equals("Instructions")) {
+
+					Element defaultInstructionsElement = defaultElementsMap.get("Instructions");
+
+					/*- ------ExecutionTicks element --------------*/
+					newExecutionTicksElement = new Element(nodeName);
+
+					Attribute typeAttribute = new Attribute("type", "am:Ticks", this.helper.getGenericNS("xsi"));
+
+					newExecutionTicksElement.getAttributes().add(typeAttribute);
+
+					/*-
+					 *    <default key="Instructions">
+							<value xsi:type="am:NeedConstant" value="20"/>
+						  </default>
+					 */
+					Element valueElement = defaultInstructionsElement.getChild("value");
+
+					Element default_executionTicksElement = createTicksElementFromNeed(valueElement, "default");
+
+					if (default_executionTicksElement != null) {
+						// Adding default ExecutionTicks element
+						newExecutionTicksElement.addContent(default_executionTicksElement);
+					}
+				} else {
+
+					if (containerType.equals("TaskScheduler") || containerType.equals("InterruptController")) {
+						// TODO: log this information
+						continue;
+					}
+					Element defaultExecutionNeedElement = defaultElementsMap.get(key);
+
+					if (newExecutionNeedsElement == null) {
+
+						newExecutionNeedsElement = new Element(nodeName);
+
+						Attribute typeAttribute = new Attribute("type", "am:ExecutionNeed",
+								this.helper.getGenericNS("xsi"));
+
+						newExecutionNeedsElement.getAttributes().add(typeAttribute);
+
+					}
+
+					/*-
+					 *    <default key="Instructions">
+					<value xsi:type="am:NeedConstant" value="20"/>
+					 </default>
+					 */
+					Element valueElement = defaultExecutionNeedElement.getChild("value");
+
+					if (valueElement != null) {
+						Element newNeedElementValue = createTicksElementFromNeed(valueElement, "value");
+
+						if (newNeedElementValue != null) {
+							Element needsElement = new Element("needs");
+
+							needsElement.setAttribute(new Attribute("key", "Instructions"));
+
+							needsElement.addContent(newNeedElementValue);
+
+							// adding needs element to ExecutionNeed element
+
+							newExecutionNeedsElement.addContent(needsElement);
+						}
+					}
+				}
+			}
+
+			/*------------------------------ Handling ExtendedElements ---------------------------*/
+
+			List<Element> extendedElements = executionNeedElement.getChildren("extended");
+
+			for (Element extendedElement : extendedElements) {
+
+				final List<Element> valueElements = this.helper.getXpathResult(extendedElement,
+						"./value[@key=\"Instructions\"]", Element.class, this.helper.getNS_093("am"));
+
+				if (valueElements.size() > 0) {
+
+					Element extended_executionTicksElement = new Element("extended");
+
+					this.helper.copyElement_Attribute_or_Element(extendedElement, extended_executionTicksElement,
+							"key");
 					/*-
 					 *  <extended key="puDef?type=ProcessingUnitDefinition">
-     						<value key="Instructions">
-     							<value xsi:type="am:NeedConstant" value="10"/>
- 						    </value>
- 						</extended>
+							<value key="Instructions">
+								<value xsi:type="am:NeedConstant" value="10"/>
+						    </value>
+						</extended>
 					 * 
 					 */
 					Element valueElement = valueElements.get(0);
-					
-					Element valueValueElement=valueElement.getChild("value");
-					
-					Element value_executionTicksElement= createTicksElementFromNeed( valueValueElement,"value");
-					
-					if(value_executionTicksElement!=null) {
+
+					Element valueValueElement = valueElement.getChild("value");
+
+					Element value_executionTicksElement = createTicksElementFromNeed(valueValueElement, "value");
+
+					if (value_executionTicksElement != null) {
 						/*- adding value to the Extended ticks element */
 						extended_executionTicksElement.addContent(value_executionTicksElement);
-						
-						//Adding extended ExecutionTicks element
-						executionTicksElement.addContent(extended_executionTicksElement);
-						
+
+						// Adding extended ExecutionTicks element
+
+						if (newExecutionTicksElement == null) {
+							newExecutionTicksElement = new Element(nodeName);
+
+							Attribute typeAttribute = new Attribute("type", "am:Ticks",
+									this.helper.getGenericNS("xsi"));
+
+							newExecutionTicksElement.getAttributes().add(typeAttribute);
+						}
+						newExecutionTicksElement.addContent(extended_executionTicksElement);
+
 					}
 				}
-			 
+
 			}
-			
+
 			Parent parent = executionNeedElement.getParent();
 
-			/*- checking if the ExecutionTicks contain any elements. If there are no children inside ExecutionTicks, it is not required to add it as a RunnableItem */
-			if(executionTicksElement.getChildren().size()>0) {
-				
-				int indexOf = parent.indexOf(executionNeedElement);
-				
-				parent.addContent(indexOf, executionTicksElement);
-				
+			if (newExecutionNeedsElement != null) {
+				parent.addContent(newExecutionNeedsElement);
+			} 
+			if (newExecutionTicksElement != null) {
+
+				parent.addContent(newExecutionTicksElement);
 			}
-			
-			final List<Element> skippedValues = this.helper.getXpathResult(executionNeedElement,"./default[not(@key=\"Instructions\")] | ./extended/value[not(@key=\"Instructions\")]", Element.class,
-					this.helper.getNS_093("am"));
-			
-			StringBuffer buffer=new StringBuffer();
-			for (Element elementKey : skippedValues) {
+
+			final List<Element> skippedValuesExtended = this.helper.getXpathResult(executionNeedElement,
+					"./extended/value[not(@key=\"Instructions\")]", Element.class, this.helper.getNS_093("am"));
+
+			final List<Element> skippedValuesDefault = this.helper.getXpathResult(executionNeedElement,
+					"./default[not(@key=\"Instructions\")]", Element.class, this.helper.getNS_093("am"));
+
+			StringBuffer extendedElementsbuffer = new StringBuffer();
+			for (Element elementKey : skippedValuesExtended) {
 				String attributeValue = elementKey.getAttributeValue("key");
-				buffer.append(attributeValue);
-				buffer.append(",");
+				extendedElementsbuffer.append(attributeValue);
+				extendedElementsbuffer.append(",");
+			}
+			StringBuffer defaultElementsbuffer = new StringBuffer();
+			for (Element elementKey : skippedValuesDefault) {
+				String attributeValue = elementKey.getAttributeValue("key");
+				defaultElementsbuffer.append(attributeValue);
+				defaultElementsbuffer.append(",");
+			}
+
+			if (extendedElementsbuffer.length() > 0) {
+				logger.info("In " + containerType + " : \"" + containerElementName
+						+ ", \" Entries of ExecutionNeed (extended elements) with following keys can not be migrated : "
+						+ extendedElementsbuffer.toString().substring(0, extendedElementsbuffer.length() - 1) +" -> (only supported \"HwFeatureCategory\" as key is : \"Instructions\")");
+
 			}
 			
-			if(buffer.length()>0) {
-				logger.info("In "+containerType+" : \""+containerElementName+"\" Entries of ExecutionNeed with following keys can not be migrated (only supported \"HwFeatureCategory\" as key is : \"Instructions\"): " + buffer.toString().substring(0, buffer.length()-1));
-				
+			if ((defaultElementsbuffer.length() > 0)&&(containerType.equals("TaskScheduler")||containerType.equals("InterruptController"))) {
+				logger.info("In " + containerType + " : \"" + containerElementName
+						+ ", \" Entries of ExecutionNeed (default elements) with following keys can not be migrated : "
+						+ defaultElementsbuffer.toString().substring(0, defaultElementsbuffer.length() - 1) +" ->(only supported \"HwFeatureCategory\" as key is : \"Instructions\")");
+
 			}
-			
-			
+
 			/*- Removing ExecutionNeeds element as the data of it is migrated */
- 
+
 			parent.removeContent(executionNeedElement);
+
 		}
 	}
 
