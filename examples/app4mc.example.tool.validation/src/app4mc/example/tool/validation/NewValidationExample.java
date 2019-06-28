21fd59be2707ee3c03e798728c2354b7f4b4147a
Harald Mackamul <harald.mackamul@de.bosch.com>
2019-06-28 14:52:18 +0200

    Updated examples to demonstrate validations and index

--- examples/app4mc.example.tool.validation/src/app4mc/example/tool/validation/NewValidationExample.java
+++ examples/app4mc.example.tool.validation/src/app4mc/example/tool/validation/NewValidationExample.java
@@ -16,8 +16,10 @@
 package app4mc.example.tool.validation;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -34,6 +36,8 @@ import org.eclipse.app4mc.amalthea.validations.AmaltheaProfile;
 import org.eclipse.app4mc.amalthea.validations.EMFProfile;
 import org.eclipse.app4mc.validation.core.IProfile;
 import org.eclipse.app4mc.validation.core.ValidationDiagnostic;
+import org.eclipse.app4mc.validation.util.ProfileManager;
+import org.eclipse.app4mc.validation.util.ValidationAggregator;
 import org.eclipse.app4mc.validation.util.ValidationExecutor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
@@ -47,7 +51,8 @@ public class NewValidationExample {
 
 		// example: relative path
 		final File inputFile = new File("model-input/democar.amxmi");
-		final File outputFile = new File("model-output/validation/issues.txt");
+		final File resultFile = new File("model-output/validation/validation-results.txt");
+		final File infoFile = new File("model-output/validation/validation-profile.txt");
 
 		// ***** Load *****
 
@@ -58,42 +63,88 @@ public class NewValidationExample {
 			return;
 		}
 
+		// ***** Prepare file writer(s) and file stream(s) *****
 
-		// ***** Validate *****
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		PrintWriter printWriter1 = null;
+		try {
+			Files.createDirectories(Paths.get(resultFile.getAbsoluteFile().getParent()));
+			FileWriter fileWriter = new FileWriter(resultFile.getAbsoluteFile(), false);
+			printWriter1 = new PrintWriter(fileWriter);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		PrintStream printStream1 = null;
+		try {
+			Files.createDirectories(Paths.get(infoFile.getAbsoluteFile().getParent()));
+			FileOutputStream fileStream = new FileOutputStream(infoFile.getAbsoluteFile(), false);
+			printStream1 = new PrintStream(fileStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("\nWriting file: " + infoFile.getAbsoluteFile());
+		
+		printStream1.println(dateFormat.format(new Date()));
+		
+		// ***** Show single profile resolution *****
+
+		printStream1.println("\n\n*** Validation Profiles ***");
+		
+		ProfileManager man = new ProfileManager();
+		
+		printStream1.println();
+		man.dumpProfile(AmaltheaProfile.class, printStream1);
+
+		printStream1.println();
+		man.dumpProfile(EMFProfile.class, printStream1);
+
+		// ***** Print (complex) profile info *****
 
 		List<Class<? extends IProfile>> profileList = new ArrayList<>(
 				Arrays.asList(AmaltheaProfile.class, EMFProfile.class));
+		
+		printStream1.println("\n\n*** Validation info ***");
+
+		ValidationAggregator aggregator = new ValidationAggregator();
+		aggregator.addProfiles(profileList);
+
+		printStream1.println();
+		printStream1.println("Compact validation map (map1)");
+		printStream1.println("-----------------------------");
+		printStream1.println();
+		aggregator.dumpValidationMap1(printStream1);
+
+		printStream1.println("\n");
+		printStream1.println("Expanded validation map (map2)");
+		printStream1.println("------------------------------");
+		printStream1.println();
+		aggregator.dumpValidationMap2(printStream1);
+
+		printStream1.close();
+
+		// ***** Validate *****
 
 		ValidationExecutor executor = new ValidationExecutor(profileList);
 		executor.validate(model, new NullProgressMonitor());
 		List<ValidationDiagnostic> results = executor.getResults();
 
-
 		// ***** Print results *****
 
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		printWriter1.println("\nValidation results (" + dateFormat.format(new Date()) + ")\n");
 
-		PrintWriter printWriter = null;
-		try {
-			Files.createDirectories(Paths.get(outputFile.getAbsoluteFile().getParent()));
-			FileWriter fileWriter = new FileWriter(outputFile.getAbsoluteFile(), false);
-			printWriter = new PrintWriter(fileWriter);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		printWriter.println("\nValidation results (" + dateFormat.format(new Date()) + ")\n\n");
-
-		System.out.println("Writing file: " + outputFile.getAbsoluteFile());
+		System.out.println("\nWriting file: " + resultFile.getAbsoluteFile());
 		for (ValidationDiagnostic result : results) {
-			printWriter.println("    " + result.getValidationID() + " -- " + result.getSeverityLevel());
-			printWriter.println("    " + result.getMessage());
-			printWriter.println("    --------------------------------");
+			printWriter1.println("    " + result.getValidationID() + " -- " + result.getSeverityLevel());
+			printWriter1.println("    " + result.getMessage());
+			printWriter1.println("    --------------------------------");
 		}
 
-		printWriter.close();
+		printWriter1.close();
 
-		System.out.println("done");
+		System.out.println("\ndone");
 	}
 
 }
