9a2df95356d85c12ef0036f3a724aa06dd3e4126
Harald Mackamul <harald.mackamul@de.bosch.com>
2019-07-22 18:56:22 +0200

    [OI2JIRO] Changed tools directory

--- build/org.eclipse.app4mc.build/hudson/init_mylyn.sh
+++ build/org.eclipse.app4mc.build/hudson/init_mylyn.sh
@@ -2,4 +2,4 @@
 script_dir=$(dirname $0)
 folder_libs=$script_dir/../../../dev_env_lib/help_docu
 mkdir -p $folder_libs
-cp -v /shared/common/mylyn-wikitext/2.9.0.v20160513-1433/*.jar $folder_libs
+cp -v /opt/tools/mylyn-wikitext/2.9.0.v20160513-1433/*.jar $folder_libs
